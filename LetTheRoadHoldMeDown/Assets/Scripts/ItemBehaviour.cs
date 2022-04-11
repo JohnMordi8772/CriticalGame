93d3e84fabfd724efc3a4a8f5dea8e2a6860c35a
tcolander <tcolander@mail.bradley.edu>
2022-04-11 13:25:40 -0500

    aaaaa

--- LetTheRoadHoldMeDown/Assets/Scripts/ItemBehaviour.cs
+++ LetTheRoadHoldMeDown/Assets/Scripts/ItemBehaviour.cs
@@ -4,26 +4,38 @@ using UnityEngine;
 
 public class ItemBehaviour : MonoBehaviour
 {
-    
-
+    public CapsuleCollider2D cc2d;
+    public GameObject Player;
+    public PlayerBehaviour pb;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        cc2d = GetComponent<CapsuleCollider2D>();
+        Player = GameObject.Find("Player");
+        pb = Player.GetComponent<PlayerBehaviour>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        cc2d.size = new Vector2(1 + (pb.itemCount *.5f), 1 + (pb.itemCount * .5f));
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.collider.tag == "Player")
         {
+            pb.itemCount++;
             Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D target)
+    {
+        if(target.tag == "Player")
+        {
+            target.transform.position = Vector3.MoveTowards(target.transform.position, transform.position, ((1 + (pb.itemCount * .1f)) * .05f));
         }
     }
 }
