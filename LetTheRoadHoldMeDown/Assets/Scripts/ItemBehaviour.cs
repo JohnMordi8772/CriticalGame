680ef42feb627c7b50fa010304e4c0ac4225d9bf
tcolander <tcolander@mail.bradley.edu>
2022-04-11 13:43:21 -0500

    ItemBehaviour

--- LetTheRoadHoldMeDown/Assets/Scripts/ItemBehaviour.cs
+++ LetTheRoadHoldMeDown/Assets/Scripts/ItemBehaviour.cs
@@ -7,6 +7,7 @@ public class ItemBehaviour : MonoBehaviour
     public CapsuleCollider2D cc2d;
     public GameObject Player;
     public PlayerBehaviour pb;
+    public bool isChecked;
 
     // Start is called before the first frame update
     void Start()
@@ -22,20 +23,27 @@ public class ItemBehaviour : MonoBehaviour
         cc2d.size = new Vector2(1 + (pb.itemCount), 1 + (pb.itemCount));
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+   /* private void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.collider.tag == "Player")
         {
             pb.itemCount++;
             Destroy(gameObject);
         }
-    }
+    }*/
 
     private void OnTriggerStay2D(Collider2D target)
     {
         if(target.tag == "Player")
         {
             target.transform.position = Vector3.MoveTowards(target.transform.position, transform.position, ((1 + (pb.itemCount * .1f)) * .03f));
+
+            if(Vector2.Distance(Player.transform.position, gameObject.transform.position) < .75f && isChecked == false)
+            {
+                isChecked = true;
+                pb.itemCount++;
+                Destroy(gameObject);
+            }
         }
     }
 }
