538e44823ef07dc2b942332d885b1f41a5c28c3d
John Mordi <johnmordi00@gmail.com>
2022-04-27 13:23:59 -0500

    Little updates

--- LetTheRoadHoldMeDown/Assets/CloseOpening.cs
+++ LetTheRoadHoldMeDown/Assets/CloseOpening.cs
@@ -7,13 +7,16 @@ public class CloseOpening : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Time.timeScale = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
         if (Input.anyKeyDown)
+        {
+            Time.timeScale = 1;
             gameObject.SetActive(false);
+        }
     }
 }
